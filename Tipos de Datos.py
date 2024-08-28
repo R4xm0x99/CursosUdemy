c64129d50e7a6a6ce431a32beebee626876a13d7
AleJo99 <51728671+R4xm0x99@users.noreply.github.com>
2024-08-28 12:27:50 -0500

    Actualizacion de tipos de variables

---
+++ Tipos de Datos.py
@@ -0,0 +1,24 @@
+# Ejemplos de tipos de datos en Python
+
+# 1. Enteros
+edad= 30
+print('Edad: ',edad)
+
+#2.Numero con punto flotante
+
+altura = 1.75
+print('Altura: ', altura)
+
+# 3. cadena de texto
+nombre='Lucas'
+print(' el nombre del sujeto es: ', nombre)
+
+#4. Tipo Boolean
+
+es_estudiante= False
+print('Es estudiantes:', es_estudiante)
+
+#5. N0ne, ausencia de valor
+
+direccion=None
+print('Direccion', direccion)
