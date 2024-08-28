0bb16ce19f940e2cb9bbc6aafa2e39095e074d80
AleJo99 <51728671+R4xm0x99@users.noreply.github.com>
2024-08-28 11:35:39 -0500

    actualizacion de reglas de variables en Python

--- Nombre de variables.py
+++ Nombre de variables.py
@@ -1,5 +1,11 @@
 #reglas y convenciones en nombre de variables
 
 #Ejemplo de regla estrictas
-
-nombre_usuario= 'Juan Perez'
+nombre= 'Juan' # las variabables se deben crear con letras minusculas y siempre con "_"
+apellido='Perez'
+#Snake case ejemplo
+nombre_completo='Marta Juarez' #Segun definicion de snake case las variables se crean con "_"
+#Prefijo y sufijos
+es_casado= True
+es_soltero=False
+nombre_txt= 'Archivo.txt'
